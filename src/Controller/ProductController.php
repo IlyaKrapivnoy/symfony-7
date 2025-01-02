253653ba7b04aeb9374dd764349dc4ff962d65ed
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2025-01-02 19:00:06 +0100

    feat: delete item logic

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -77,4 +77,21 @@ class ProductController extends AbstractController
             'form' => $form,
         ]);
     }
+
+    #[Route('/products/{id<\d+>}/delete', name: 'product_delete')]
+    public function delete(Request $request, Product $product, EntityManagerInterface $manager): Response
+    {
+        if($request->isMethod('POST')) {
+            $manager->remove($product);
+            $manager->flush();
+
+            $this->addFlash('notice', 'Product deleted successfully!');
+
+            return $this->redirectToRoute('product_index');
+        }
+
+        return $this->render('product/delete.html.twig', [
+            'id' => $product->getId(),
+        ]);
+    }
 }
