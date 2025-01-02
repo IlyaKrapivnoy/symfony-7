05cc819add6ceb2669ebb526bcd1dedee854559b
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2025-01-02 17:53:49 +0100

    feat: add form

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\ProductRepository;
 use App\Entity\Product;
+use App\Form\ProductType;
 
 class ProductController extends AbstractController
 {
@@ -29,4 +30,12 @@ class ProductController extends AbstractController
         ]);
     }
 
+    #[Route('/products/new', name: 'product_new')]
+    public function new(): Response
+    {
+        $form = $this->createForm(ProductType::class);
+        return $this->render('product/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
 }
